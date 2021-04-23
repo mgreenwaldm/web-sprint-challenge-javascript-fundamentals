16222cfc7d6295277d19d7d24fd7f7b8e99a98b9
Michael Greenwald <mgreenwaldm@icloud.com>
2021-04-23 12:22:38 -0500

    completed project

--- index.js
+++ index.js
@@ -112,8 +112,11 @@ displayNames will be an array of strings, and each string should follow this pat
 */
 
 function animalNames(animals) {
-  return animals.map(v => `name: ${v.animal_name}, scientific: ${v.scientific_name}`);
+  return animals.map(currentMapValue => `name: ${currentMapValue.animal_name}, scientific: ${currentMapValue.scientific_name}`);
 }
+
+
+animalNames(zooAnimals);
 
 /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
 The zoo needs a list of all their animal's names converted to lower case. 
@@ -155,28 +158,28 @@ function USApop(animals) {
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
 
-function consume(a, b, c) {
-  return c(a, b);
+function consume(a, b, cb) {
+  return cb(a, b);
 }
 
 
 /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
 // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
 
-function add( /*Your Code Here */ ) {
-  /*Your Code Here*/
+function add(a, b) {
+  return a + b;
 }
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
 
-function multiply( /*Your Code Here */ ) {
-  /*Your Code Here */
+function multiply(a, b) {
+  return a * b;
 }
 
 // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
 
-function greeting( /*Your Code Here */ ) {
-  return /*Your Code Here */
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`
 }
 
 // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
@@ -197,8 +200,14 @@ function greeting( /*Your Code Here */ ) {
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker( /*Your Code Here */ ) {
-  /*Your Code Here */
+function CuboidMaker({
+  length,
+  width,
+  height
+}) {
+  this.length = length;
+  this.width = width;
+  this.height = height;
 }
 
 
@@ -206,7 +215,9 @@ function CuboidMaker( /*Your Code Here */ ) {
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
 
-
+CuboidMaker.prototype.volume = function () {
+  return this.length * this.width * this.height;
+}
 
 
 
@@ -215,7 +226,9 @@ function CuboidMaker( /*Your Code Here */ ) {
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function () {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
 
 
 
@@ -223,7 +236,11 @@ function CuboidMaker( /*Your Code Here */ ) {
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+let cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+})
 
 
 
@@ -236,8 +253,29 @@ function CuboidMaker( /*Your Code Here */ ) {
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo {
+  constructor({
+    length,
+    width,
+    height
+  }) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
 
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
 }
+
+let cuboidTwo = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+})
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
